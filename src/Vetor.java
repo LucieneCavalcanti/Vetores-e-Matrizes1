b9c0aede7b49572933f33ca7b9323db40278e043
lucienecavalcanti <lucienecavalcanti@live.com>
2023-09-01 20:04:02 -0300

    Versão 2

--- src/Vetor.java
+++ src/Vetor.java
@@ -2,12 +2,19 @@ import java.util.Scanner;
 
 public class Vetor {
     public static void main(String[] args) {
-        float notas[] = new float[6];
+        int qtdeNotas = 0;
         float somaNotas=0, media=0;
         Scanner entrada = new Scanner(System.in);
+        do {
+            System.out.println("Digite a quantidade de notas:");
+            qtdeNotas = entrada.nextInt();
+        } while (qtdeNotas<=0);
+        float notas[] = new float[qtdeNotas];
         for (int i = 0; i < notas.length; i++) {
-            System.out.println("Digite a nota "+ (i+1) +":");
-            notas[i] = entrada.nextFloat();
+            do{ //validação
+                System.out.println("Digite a nota "+ (i+1) +":");
+                notas[i] = entrada.nextFloat();
+            }while(notas[i]<0 || notas[i]>10);
             somaNotas+=notas[i];//somaNotas=somaNotas+notas[i]
         }
         //somaNotas = notas[0]+notas[1]+notas[2]+notas[3];
